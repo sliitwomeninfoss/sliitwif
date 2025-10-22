ecdcd86385ee9a6fee99752dbb4a18ec0afd75b4
Nujaba Irfan <nujaba30@gmail.com>
2025-10-22 05:51:21 +0530

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
+const path = require("path");
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   basePath: '/sliitwif',
   assetPrefix: '/sliitwif/',
+   outdir: path.join(__dirname, 'out')
 };
 
 export default nextConfig;
