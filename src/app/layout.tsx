8e328209e3fee46ebabc970a18bb538df2ccbcb2
Sandali <sandalisandagomi@gmail.com>
2026-06-20 22:24:52 +0530

    feat: Add RecruitmentPopup component to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/footer";
+import RecruitmentPopup from "./components/RecruitmentPopup";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+        <RecruitmentPopup />
         <main>{children}</main>
         <Footer />
       </body>
