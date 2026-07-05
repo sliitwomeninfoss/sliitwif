f2ab96dc0e362d6fb0477e287eed8ee06a4f269f
Sandali <sandalisandagomi@gmail.com>
2026-07-05 14:52:50 +0530

    fix: Remove unused RecruitmentPopup component from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/footer";
-import RecruitmentPopup from "./components/RecruitmentPopup";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +34,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <RecruitmentPopup />
         <main>{children}</main>
         <Footer />
       </body>
